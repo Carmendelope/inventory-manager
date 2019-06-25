170d9cd02cc54dd0cc389f3377c4db0f21dee3da
Giel de Nijs <giel@daisho.group>
2019-06-25 09:45:30 -0700

    [NP-1520] Storing, merging and aggregating metrics from multiple ECs.

--- internal/pkg/server/monitoring/manager.go
+++ internal/pkg/server/monitoring/manager.go
@@ -5,6 +5,7 @@
 package monitoring
 
 import (
+	"sort"
 	"time"
 
 	"github.com/nalej/derrors"
@@ -79,17 +80,15 @@ func (m *Manager) QueryMetrics(request *grpc_inventory_manager_go.QueryMetricsRe
 		return nil, conversions.ToGRPCError(derr)
 	}
 
-	// TODO [NP-1520]
-	// For now, we only allow metrics from a single edge controller
-	// - Create a request for each Edge Controller and execute
-	// - Unify the results
+	aggregationType := request.GetAggregation()
 
-	if len(selectors) > 1 {
-		return nil, derrors.NewUnimplementedError("Querying metrics from more than one edge controller not supported")
-	}
+	// Results is a mapping from metric to values, where values is a mapping
+	// from timestamp to value and count. This last mapping is needed for merging
+	// results from multiple edge controllers. We will convert to one
+	// QueryMetricsResult to return afterwards
+	results := make(map[string]map[int64]*grpc_inventory_manager_go.QueryMetricsResult_Value)
 
-	results := make([]*grpc_inventory_manager_go.QueryMetricsResult, 0, len(selectors))
-
+	// Request for each Edge Controller and execute
 	for _, selector := range(selectors) {
 		proxyRequest := &grpc_inventory_manager_go.QueryMetricsRequest{
 			Assets: selector,
@@ -101,19 +100,100 @@ func (m *Manager) QueryMetrics(request *grpc_inventory_manager_go.QueryMetricsRe
 		log.Debug().Interface("request", proxyRequest).Msg("proxy request for QueryMetrics")
 		ctx, _ := contexts.ProxyContext()
 		result, err := m.proxyClient.QueryMetrics(ctx, proxyRequest)
+
+		// Optimization when we're querying only a single EC, in which
+		// case we can also return any errors
+		if len(selectors) == 1 {
+			log.Debug().Str("ecid", ecId).Msg("querying single edge controller - skipping merging")
+			return result, err
+		}
+
 		if err != nil {
 			// We still want to query to working edge controllers
 			log.Warn().Str("edge-controller-id", ecId).Err(err).Msg("failed calling QueryMetrics")
 			continue
 		}
-		results = append(results, result)
+
+		// Loop over all returned metrics
+		for metric, assetMetrics := range(result.GetMetrics()) {
+			// Currently, we always have a single assetMetric
+			assetMetricList := assetMetrics.GetMetrics()
+			if len(assetMetricList) == 0 {
+				continue
+			}
+			if len(assetMetricList) > 1 {
+				log.Warn().Msg("received query result for more than one individual asset - not supported")
+			}
+
+			assetMetric := assetMetricList[0]
+			assetMetricValues := assetMetric.GetValues()
+
+			// Create or retrieve map for this metric
+			values, found := results[metric]
+			if !found {
+				values = make(map[int64]*grpc_inventory_manager_go.QueryMetricsResult_Value, len(assetMetricValues))
+				results[metric] = values
+			}
+
+			log.Debug().Str("ecid", ecId).Str("metric", metric).Int("count", len(assetMetricValues)).Msg("storing metrics")
+			for _, assetMetricValue := range assetMetricValues {
+				// Create or add to value for this timestamp
+				timestamp := assetMetricValue.GetTimestamp()
+				value, found := values[timestamp]
+				if !found {
+					values[timestamp] = assetMetricValue
+				} else {
+					value.Value += assetMetricValue.Value
+					value.AssetCount += assetMetricValue.AssetCount
+				}
+			}
+		}
 	}
 
-	// We only have one - if we have anything
-	if len(results) == 0 {
-		return &grpc_inventory_manager_go.QueryMetricsResult{}, nil
+	// Unify the results
+	metricResults := make(map[string]*grpc_inventory_manager_go.QueryMetricsResult_AssetMetrics, len(results))
+	for metric, valueMap := range(results) {
+		// Make a list sorted timestamps
+		keys := make([]int64, len(valueMap))
+		for key := range(valueMap) {
+			keys = append(keys, key)
+		}
+		// Sort int64
+		sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+
+		log.Debug().Str("metric", metric).Int("count", len(keys)).Msg("aggregating metrics")
+		// Create final value list, applying aggregation if needed
+		values := make([]*grpc_inventory_manager_go.QueryMetricsResult_Value, 0, len(keys))
+		for _, key := range(keys) {
+			value := valueMap[key]
+			switch aggregationType {
+			case grpc_inventory_manager_go.AggregationType_SUM:
+				// Nothing to do - it's already a sum
+			case grpc_inventory_manager_go.AggregationType_AVG:
+				value.Value = value.Value / value.AssetCount
+			default:
+				return nil, derrors.NewInvalidArgumentError("unknown aggregation type").WithParams(aggregationType)
+			}
+			values = append(values, value)
+		}
+
+		metricResults[metric] = &grpc_inventory_manager_go.QueryMetricsResult_AssetMetrics{
+			Metrics: []*grpc_inventory_manager_go.QueryMetricsResult_AssetMetricValues{
+				&grpc_inventory_manager_go.QueryMetricsResult_AssetMetricValues{
+					// We don't have an asset id - if we had a single
+					// asset, we would have had a single EC, and we
+					// would have returned already.
+					Values: values,
+					Aggregation: aggregationType,
+				},
+			},
+		}
 	}
-	return results[0], nil
+
+	result := &grpc_inventory_manager_go.QueryMetricsResult{
+		Metrics: metricResults,
+	}
+	return result, nil
 }
 
 // GetSelectors will turn a single asset selector into a selector per
