0a4de8a266c5c8650d90e0e891dcfa7b320e78ac
Gaizka Díaz <gdiaz@nalej.com>
2019-11-07 18:05:22 +0100

    NP-2142 repo prepared for open source

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,5 +1,17 @@
 /*
- * Copyright (C) 2019 Nalej - All Rights Reserved
+ * Copyright 2019 Nalej
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package config
@@ -17,7 +29,7 @@ type Config struct {
 	Debug bool
 	// Port where the gRPC API service will listen requests.
 	Port int
-	//DsnURL with the host to configure dns
+	// DsnURL with the host to configure dns
 	DnsURL string
 	// VPNManagerAddress with the host:port to connect to the VPN manager.
 	VPNManagerAddress string
--- internal/pkg/server/service.go
+++ internal/pkg/server/service.go
@@ -1,5 +1,17 @@
 /*
- * Copyright (C) 2019 Nalej - All Rights Reserved
+ * Copyright 2019 Nalej
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package server
@@ -56,7 +68,7 @@ type BusClients struct {
 }
 
 func (s *Service) GetBusClients() (*BusClients, derrors.Error) {
-	queueClient := pulsar_comcast.NewClient(s.Configuration.QueueAddress)
+	queueClient := pulsar_comcast.NewClient(s.Configuration.QueueAddress, nil)
 
 	// inventory Events Consumer
 	invEventsOpts := events.NewConfigInventoryEventsConsumer(1, events.ConsumableStructsInventoryEventsConsumer{
